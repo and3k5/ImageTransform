f2ba61dc23e362ca21f0bff562865332b3485d99
Anders K <and3k5@users.noreply.github.com>
2025-07-03 12:20:51 +0200

    WIP: Color sorting

--- src/core/algorithms/index.ts
+++ src/core/algorithms/index.ts
@@ -1,5 +1,6 @@
 import { sortbw } from "./sortbw";
 import { sortbwINVERT } from "./sortbw/inverted";
 import { sortbwMODULO } from "./sortbw/modulo-mess";
+import { sortcolor } from "./sortcolor";
 
-export const algos = [sortbw, sortbwINVERT, sortbwMODULO];
+export const algos = [sortbw, sortbwINVERT, sortbwMODULO, sortcolor];
---
+++ src/core/algorithms/sortcolor/index.ts
@@ -0,0 +1,59 @@
+import { setDestination } from "../../Pixel";
+
+function angleDifference(a: number, b: number): number {
+    const diff = Math.abs(a - b) % 360;
+    return diff > 180 ? 360 - diff : diff;
+}
+type IndexAndHue = [number, number];
+
+function getIndexAndHue(d: ImageData) {
+    return [].map.bind(new Uint32Array(d.data.buffer))(function (a, b) {
+        const rgb = getHSLFromUint8ArrayPart(d.data, b);
+        return [b, rgb.h] as IndexAndHue;
+    });
+}
+
+import type { Algo } from "../Algo";
+import { getHSLFromUint8ArrayPart, imageDataToRenderTable } from "../commons";
+
+export const sortcolor: Algo = {
+    name: "Sort by color",
+    id: "sortcolor",
+    run: function (imgdata1, imgdata2, renderTable, state) {
+        imageDataToRenderTable(imgdata1, state, renderTable);
+        const indexAndHue1 = getIndexAndHue(imgdata1);
+        const indexAndHue2 = getIndexAndHue(imgdata2);
+
+        const len = renderTable.length;
+        const img2w = imgdata2.width;
+        const img2h = imgdata2.height;
+        for (let i = 0; i < len; i++) {
+            const iH1 = indexAndHue1[0];
+            const rT = renderTable[i];
+            const iH2 = indexAndHue2.reduce(
+                (prev, curr) => {
+                    if (prev === undefined) {
+                        return curr;
+                    }
+                    if (
+                        Math.abs(angleDifference(prev[1], iH1[1])) >
+                        Math.abs(angleDifference(curr[1], iH1[1]))
+                    ) {
+                        return curr;
+                    }
+                    return prev;
+                },
+                undefined as undefined | IndexAndHue,
+            );
+            if (iH2 == null) throw new Error("didnt find pixel to match");
+            indexAndHue2.splice(indexAndHue2.indexOf(iH2), 1);
+            const nRT = iH2[1];
+            setDestination(rT, {
+                x: (state.WIDTH - img2w) / 2 + (nRT % img2w),
+                y: (state.HEIGHT - img2h) / 2 + ~~(nRT / img2w),
+            });
+        }
+
+        console.log(indexAndHue2);
+    },
+};
