8258a1b3303fb6a8eb9457403cf18de39c2bfd19
Anders <and3k5@users.noreply.github.com>
2025-07-03 22:37:25 +0200

    Make algoid as route query and prop

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import InputImage from "./components/InputImage.vue";
 import { type InputImageValue } from "./types/InputImageValue";
 import { algos } from "./core/algorithms";
 import { createConvertor } from "./core/it";
 import { useColorMode } from "./color-mode-store";
+import { useRouter } from "vue-router";
 
 const CONVERTOR = createConvertor();
+
+const props = defineProps<{
+    algoid: string;
+}>();
+
+const router = useRouter();
+
+function updateAlgo(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    if (target.value === "") {
+        router.push({ query: { algoid: undefined } });
+    } else {
+        router.push({ query: { algoid: target.value } });
+    }
+}
 
 const imageValue1 = ref<InputImageValue>({
     imageData: null!,
@@ -18,18 +34,7 @@ const imageValue2 = ref<InputImageValue>({
 
 const canvasContainer = ref<HTMLDivElement>();
 
-const algorithms = ref<HTMLSelectElement>();
 const colorMode = useColorMode();
-
-onMounted(() => {
-    algos.forEach(function (algo) {
-        const option = new Option();
-        option.value = algo.id;
-        option.textContent = algo.name;
-        //console.log("Added algo: " + algo.name);
-        algorithms.value!.add(option);
-    });
-});
 
 function startConvert() {
     if (canvasContainer.value == null) throw new Error("canvasContainer is missing");
@@ -41,8 +46,11 @@ function startConvert() {
         canvasContainer.value.removeChild(canvasContainer.value.children[0]);
     canvasContainer.value.appendChild(canv1);
     // <canvas id="canv1" ref="canv1" @click="CONVERTOR.render()"></canvas>
+    if (props.algoid == null) {
+        throw new Error("Algorithm ID is missing");
+    }
     CONVERTOR.CONVERT(
-        algorithms.value!.value,
+        props.algoid,
         canv1,
         imageValue1.value.imageData,
         imageValue2.value.imageData,
@@ -100,8 +108,13 @@ function startConvert() {
                             <select
                                 class="form-select form-select-lg mb-3"
                                 id="algoritms"
-                                ref="algorithms"
-                            ></select>
+                                :value="algoid"
+                                @input="updateAlgo($event)"
+                            >
+                                <option v-for="algo in algos" :value="algo.id" :key="algo.id">
+                                    {{ algo.name }}
+                                </option>
+                            </select>
                             <div class="row">
                                 <InputImage
                                     label="Select image 1"
--- src/core/algorithms/index.ts
+++ src/core/algorithms/index.ts
@@ -2,4 +2,6 @@ import { sortbw } from "./sortbw";
 import { sortbwINVERT } from "./sortbw/inverted";
 import { sortbwMODULO } from "./sortbw/modulo-mess";
 
+export const defaultAlgoId = sortbw.id;
+
 export const algos = [sortbw, sortbwINVERT, sortbwMODULO];
